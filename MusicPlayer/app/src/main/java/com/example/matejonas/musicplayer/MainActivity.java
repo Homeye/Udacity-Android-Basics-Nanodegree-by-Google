4a5b170b9662abf526606459fd3eb165bc4b1c97
Mate Jonas <mate.jonas@accenture.com>
2018-04-16 14:22:17 +0200

    added start/pause buttons and music. everything works

--- MusicPlayer/app/src/main/java/com/example/matejonas/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/matejonas/musicplayer/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.matejonas.musicplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mMediaplyer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMediaplyer = MediaPlayer.create(this, R.raw.bensoundhighoctane);
+
+        Button playButton = findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMediaplyer.start();
+            }
+        });
+
+        Button pauseButton = findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMediaplyer.pause();
+            }
+        });
     }
 }
