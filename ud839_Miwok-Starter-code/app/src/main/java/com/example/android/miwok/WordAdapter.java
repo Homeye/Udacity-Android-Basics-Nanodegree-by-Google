c52cb8570531e64f474b2b1666070242edfe40e4
Mate Jonas <mate.jonas@accenture.com>
2018-04-16 11:55:46 +0200

    finished the colors/styles

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,19 +3,24 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(@NonNull Activity context, @NonNull ArrayList<Word> words) {
+    private int mColorResourceId;
+
+    public WordAdapter(@NonNull Activity context, @NonNull ArrayList<Word> words, int colorResourceId) {
         super(context, 0, words);
+        mColorResourceId = colorResourceId;
     }
 
     @NonNull
@@ -46,6 +51,10 @@ public class WordAdapter extends ArrayAdapter<Word> {
         } else {
             imageView.setVisibility(View.GONE);
         }
+
+        LinearLayout textContainer = (LinearLayout) listItemView.findViewById(R.id.text_container);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        textContainer.setBackgroundColor(color);
 
         return listItemView;
     }
