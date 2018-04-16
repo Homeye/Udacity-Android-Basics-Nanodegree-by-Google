42b0bb78b14dd9e417b48a1320fd52ef351e30ed
Mate Jonas <mate.jonas@accenture.com>
2018-04-16 16:59:32 +0200

    created an oncompletion callback method changed the audio file to a shorter one

--- MusicPlayer/app/src/main/java/com/example/matejonas/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/matejonas/musicplayer/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMediaplyer = MediaPlayer.create(this, R.raw.bensoundhighoctane);
+        mMediaplyer = MediaPlayer.create(this, R.raw.nokia_scratch_sms);
 
         Button playButton = findViewById(R.id.play_button);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mMediaplyer.start();
+                mMediaplyer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        Toast.makeText(MainActivity.this, "I'm done!", Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
 
